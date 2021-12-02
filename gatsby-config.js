3d4dc4ae0eab9616a8d835fa0262b2ebe0e360ad
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-12-02 20:18:05 +0530

    Update blog boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Fayaz Ahmed`,
+      summary: `Open sourcing at Chatwoot`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://fayaz.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `fayazara`,
     },
   },
   plugins: [
